d2ad88dfe41834e3a594aa89ea956ce2609abf4e
Doug Kintop <dkintop@hotmail.com>
2020-01-11 20:20:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+}
+
+a = new Polygon([1, 2, 1, 2]);
+console.log(a.countSides());
